bac21bb3dd6c6dd90891dc65ed7afa0678b0b3d4
satyanmvv <satyan.m.v.v@gmail.com>
2021-01-31 00:08:59 -0500

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
     
 	public static void main(String[] args) {
-		System.out.println("testing2");
+		System.out.println("A");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
